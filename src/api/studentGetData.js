257ab6540688acbf81c7a9c66a084218cf391c30
lhzandyqh <18627541631@163.com>
2019-05-29 14:53:39 +0800

    加入接口文件

---
+++ src/api/studentGetData.js
@@ -0,0 +1,73 @@
+import request from '@/utils/request'
+
+// 获取用户信息
+export function getUserInformation (parems) {
+  return request({
+    url: '/api/index/student_info',
+    method: 'get',
+    params: parems
+  })
+}
+
+// 获取用户成绩信息
+export function getUserGrade (parems) {
+  return request({
+    url: '/api/index/score_info',
+    method: 'get',
+    params: parems
+  })
+}
+
+// 获取考试信息
+export function getExamInformation (parems) {
+  return request({
+    url: '/api/index/exam_info',
+    method: 'get',
+    params: parems
+  })
+}
+
+// 获取班级成绩超越率
+export function getClassGradeByondRate (parems) {
+  return request({
+    url: '/api/byondrate/class_byond_rate',
+    method: 'get',
+    params: parems
+  })
+}
+
+// 获取学校成绩超越率
+export function getSchoolGradeByondRate (parems) {
+  return request({
+    url: '/api/byondrate/school_byond_rate',
+    method: 'get',
+    params: parems
+  })
+}
+
+// 获取班级成绩
+export function getClassGradeTable (parems) {
+  return request({
+    url: '/api/score/class_score',
+    method: 'get',
+    params: parems
+  })
+}
+
+// 获取全科成绩
+export function getSubjectGradeTable (parems) {
+  return request({
+    url: '/api/score/single_score',
+    method: 'get',
+    params: parems
+  })
+}
+
+// 获取历次成绩
+export function getSuccessiveGradeTable (parems) {
+  return request({
+    url: '/api/score/exams_score',
+    method: 'get',
+    params: parems
+  })
+}
