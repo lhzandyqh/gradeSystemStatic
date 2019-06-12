e404e858f9a1c6bb891a7b50bc8d4aeb752b1cc1
lhzandyqh <18627541631@163.com>
2019-06-12 11:36:09 +0800

    合并代码

--- src/student/components/tables/beyondSelfTable.vue
+++ src/student/components/tables/beyondSelfTable.vue
@@ -38,7 +38,7 @@
       width="120">
     </el-table-column>
     <el-table-column
-      prop="newclassrank"
+      prop="aimsClassIndex"
       align="center"
       label="新班级排名"
       width="120">
@@ -50,7 +50,7 @@
       width="120">
     </el-table-column>
     <el-table-column
-      prop="newschoolrank"
+      prop="aimsSchoolIndex"
       align="center"
       label="新学校排名"
       width="120">
@@ -66,7 +66,7 @@
 </template>
 
 <script>
-// import {getNewGradeRank} from '~/api/studentGetData'
+import {getNewGradeRank} from '@/api/studentGetData'
 export default {
   name: 'beyondSelfTable',
   props: {
@@ -77,21 +77,39 @@ export default {
   },
   data () {
     return {
+      input: ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+      newData: [],
       imgUrl: require('../../../../static/images/jiantou.png'),
-      input: ['', '', '', '', '', '', '', '', ''],
       newRank: ['', '']
     }
   },
   methods: {
     inquireNewRank: function (index, content) {
-      this.$emit('updateprop', this.input[index])
-      console.log(this.input[index])
-      console.log(content.subjectName)
-      // const prams = {
-      //   userID: 1,
-      //   subjectName: content.subjectName,
-      //   subjectAimsScore: this.input[index],
-      // }
+      const prams = {
+        userID: 1,
+        yuwen: this.input[1],
+        shuxue: this.input[2],
+        yingyu: this.input[3],
+        wuli: this.input[4],
+        huaxue: this.input[5],
+        shenguw: this.input[6],
+        dili: this.input[7],
+        lishi: this.input[8],
+        zhengzhi: this.input[9]
+      }
+      // console.log('调用之前')
+      // console.log(this.newData)
+      getNewGradeRank(prams).then(response => {
+        this.newData = response.data.info
+        console.log('这是传给父组件的新数据')
+        console.log(this.newData)
+        this.$emit('updateprop', this.newData)
+      })
+      // this.$emit('updateprop', this.newData)
+      // console.log(this.input[index])
+      // console.log(content.subjectName)
+      // console.log('这是传给父组件的新数据')
+      // console.log(this.newData)
     }
   }
 }
--- src/student/views/firstnavigation/beyondsefl.vue
+++ src/student/views/firstnavigation/beyondsefl.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="container">
-<!--    <h1>这是超越自我模块</h1>-->
+    <!--    <h1>这是超越自我模块</h1>-->
     <el-row>
       <h4>2017-2018学年第一学期七年级期中考试</h4>
       <span style="font-weight: bold;color: #19c237">─理想成绩─</span><br><br>
       <span style="text-align: center;font-size: 14px">曾经有珍贵的5分放在我面前我没有珍惜，如果再给我一次机会，我会说“不要再马虎了</span>
     </el-row>
-<!--    <el-row>-->
-<!--      <div class="picturecontainer">-->
-<!--        <img v-bind:src="imgUrl" width="80%">-->
-<!--      </div>-->
-<!--    </el-row>-->
+    <!--    <el-row>-->
+    <!--      <div class="picturecontainer">-->
+    <!--        <img v-bind:src="imgUrl" width="80%">-->
+    <!--      </div>-->
+    <!--    </el-row>-->
     <el-row>
-      <beyond-self-table :subjectTableData="subjectTableData"></beyond-self-table>
+      <beyond-self-table :subjectTableData="subjectTableData" v-on:updateprop = 'zidingyi($event)'></beyond-self-table>
     </el-row>
   </div>
 </template>
 
 <script>
-import beyondSelfTable from '~/components/tables/beyondSelfTable'
-import {getSubjectGradeTable} from '~/api/studentGetData'
+import beyondSelfTable from '@/components/tables/beyondSelfTable'
+import {getSubjectGradeTable} from '@/api/studentGetData'
 export default {
   name: 'beyondsefl',
   components: {beyondSelfTable},
@@ -40,6 +40,9 @@ export default {
       getSubjectGradeTable(prams).then(response => {
         this.subjectTableData = response.data.info
       })
+    },
+    zidingyi: function (newData) {
+      this.subjectTableData = newData
     }
   }
 }
