8da183160a102e6c62df241e7d9aec9438b01062
Little-God <377954252@qq.com>
2019-06-18 18:00:19 +0800

    登录

--- src/teacher/views/login/index.vue
+++ src/teacher/views/login/index.vue
@@ -1,27 +1,26 @@
 <template>
   <div>
     <div class="login-all">
-      <img src="../../assets/jpg/login-logo.jpg" class="login-logo" alt>
-      <div class="login_">
-        <img src="../../assets/jpg/login.jpg" class="login-bg" alt>
-        <div class="login-input">
-          <div class="login-center">
-            <el-input
-              v-model="username"
-              class="input_"
-              placeholder="请输入用户名"
-              prefix-icon="el-icon-user"
-            ></el-input>
-            <el-input
-              v-model="password"
-              class="input_"
-              placeholder="请输入密码"
-              prefix-icon="el-icon-attract"
-            ></el-input>
-            <el-button type="primary" class="input_" style="margin-top:20px;" @click="loginBtn">登录</el-button>
-          </div>
-          <span class="Password">忘记密码？</span>
+      <img src="../../assets/jpg/login-logo.png" class="login-logo" alt>
+
+      <img src="../../assets/jpg/login_b_t.jpg" class="login-bg" alt>
+      <div class="login-input">
+        <div class="login-center">
+          <el-input
+            v-model="username"
+            class="input_"
+            placeholder="请输入用户名"
+            prefix-icon="el-icon-user"
+          ></el-input>
+          <el-input
+            v-model="password"
+            class="input_"
+            placeholder="请输入密码"
+            prefix-icon="el-icon-attract"
+          ></el-input>
+          <el-button type="primary" class="input_" style="margin-top:20px;" @click="loginBtn">登录</el-button>
         </div>
+        <span class="Password">忘记密码？</span>
       </div>
     </div>
   </div>
@@ -56,12 +55,9 @@ export default {
         username: this.username,
         password: this.password
       }
-
       login(prams).then(res => {
         if (res.data.errno === 200) {
           if (res.data.rolename === '学生') {
-            // let localhost = 'localhost/teacher.html#/fisrtpage'
-            // console.log(localhost, 'test')
             window.location.href = '/student.html#/fisrtpage'
           }
           if (res.data.rolename === '任课教师') {
@@ -74,12 +70,15 @@ export default {
 }
 </script>
 <style >
-.login_ {
-  position: relative;
+
+.login-bg{
   width: 100%;
-  height: auto;
+  height: 100%;
 }
 .login-logo {
+ position: absolute;
+ top: 0;
+ left: 0;
   width: 550px;
   height: 100px;
   margin: 20px;
@@ -96,22 +95,20 @@ body {
   margin: 0;
 }
 .login-all {
+  position: relative;
   width: 100%;
+  height: 800px;
 }
 .login-center {
   text-align: center;
   margin: 100px auto;
-}
-.login {
-  width: 100%;
-  height: 100%;
 }
 .login-input {
   position: absolute;
   width: 500px;
   height: 460px;
   background: #fff;
-  border-radius: 20px;
+  border-radius: 10px;
   bottom: 100px;
   right: 150px;
 }
