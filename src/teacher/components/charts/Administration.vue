678675d645452683216d85f348c3a64b0a802a98
Little-God <377954252@qq.com>
2019-07-07 08:00:09 +0800

    柱状图

--- src/teacher/components/charts/Administration.vue
+++ src/teacher/components/charts/Administration.vue
@@ -88,7 +88,7 @@ export default {
     },
 
     setOptions({ expectedData, actualData } = {}) {
-        let dataAxis = ['点', '击', '柱', '子', '或', '者', '两', '指', '在', '触', '屏', '上', '滑', '动', '能', '够', '自', '动', '缩', '放'];
+        let dataAxis = [220, 182, 191, 234, 290, 330, 310, 123, 442, 321, 90, 149, 210, 122, 133, 334, 198, 123, 125, 220];
 var data = [220, 182, 191, 234, 290, 330, 310, 123, 442, 321, 90, 149, 210, 122, 133, 334, 198, 123, 125, 220];
 var yMax = 500;
 var dataShadow = [];
@@ -97,7 +97,7 @@ for (var i = 0; i < data.length; i++) {
 }
       this.chart.setOption({
         title: {
-          text: "特性示例：渐变色 阴影 点击缩放",
+          text: "─ 10分一档统计表 ─",
           subtext: "Feature Sample: Gradient Color, Shadow, Click Zoom"
         },
         xAxis: {
