5f9c54aa32531e6c2ca764c73e38bc396b1c1944
Little-God <377954252@qq.com>
2019-07-07 17:38:44 +0800

    修改

--- src/teacher/views/firstnavigation/classAdministration.vue
+++ src/teacher/views/firstnavigation/classAdministration.vue
@@ -29,11 +29,6 @@
 </template>
 <script>
 import tableTitle from "@/components/tables/tableTitle";
-<<<<<<< HEAD
-import allClassGradeTable from '@/components/tables/allClassGradeTable'
-import { getClassGradeTable,AdminClassClassScore } from '@/api/studentGetData'
-import Administration from '@/components/charts/Administration'
-=======
 import allClassGradeTable from "@/components/tables/allClassGradeTable";
 import focuson from "@/components/tables/focuson";
 import administrationContrast from "@/components/tables/administrationContrast"; // 班校对比表格
@@ -41,7 +36,6 @@ import { getClassGradeTable } from "@/api/studentGetData";
 import Administration from "@/components/charts/Administration";
 import pieChart from "@/components/charts/pie-chart";
 import grade from "@/components/tables/grade";
->>>>>>> master
 export default {
   components: {
     tableTitle,
@@ -57,42 +51,6 @@ export default {
       chartData: {},
       dataList: {},
       activeName: "first",
-<<<<<<< HEAD
-      name:'行政班各科分析表',
-       allGradeTableData: [],
-        tableInfo: [
-        { prop: 'id', lable: '序号' },
-        { prop: 'studentMachineCard', lable: '考号' },
-        {prop: 'studentName', lable: '姓名'},
-        { prop: 'classId', lable: '班级/行政班' },
-        { prop: 'coversionTotal', lable: '总分' },
-        { prop: 'classIndex', lable: '班名次' },
-        { prop: 'schoolIndex', lable: '校名次' },
-        { prop: 'advancefall', lable: '进步/后退' },
-        { prop: 'yuwenScore', lable: '语文' },
-        { prop: 'shuxueScore', lable: '数学' },
-        { prop: 'yingyuScore', lable: '英语' },
-        { prop: 'threeScore', lable: '三科总分' },
-        { prop: 'physics', lable: '物理' },
-        { prop: 'huaxueCoversion', lable: '化学' },
-        { prop: 'shengwuCoversion', lable: '生物' },
-        { prop: 'lishiCoversion', lable: '历史' },
-        { prop: 'diliCoversion', lable: '地理' },
-        { prop: 'zhengzhiCoversion', lable: '政治' }
-      ]
-    };
-  },
-  methods:{
-       getGradeTableData: function () {
-         
-      const prams = {
-        classname: '1',
-        gradename:'高二'
-      }
-      AdminClassClassScore(prams).then(response => {
-        this.allGradeTableData = response.data.info
-      })
-=======
       name: "行政班各科分析表",
       name2: "重点关注",
       name3: "行政班成绩六率分析",
@@ -128,7 +86,6 @@ export default {
       getClassGradeTable(prams).then(response => {
         this.allGradeTableData = response.data.info;
       });
->>>>>>> master
     }
   },
   mounted() {
